430479db9a05e723eff0ae1ba8d9d8a54f6cd289
EndOverdosing <endoverdosing@gmail.com>
2025-09-09 01:07:05 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,7 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-    return;
+  event.waitUntil(self.clients.claim());
 });
